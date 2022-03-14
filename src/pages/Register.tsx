69c134a7d05b197bc39e2afb569707297899d745
91jzhu <j1712032377@163.com>
2022-03-14 17:09:35 +0800

    引入 antd 表单

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,18 +1,74 @@
 import {observer} from "mobx-react";
 import useStores from "../store";
 import React from "react";
+import {Form, Input, Button, Checkbox} from 'antd';
+import styled from "styled-components";
 
-const Register=observer(()=>{
-    const {AuthStore}=useStores()
-    const onChange=(e:any)=>{
-        AuthStore.setUserName(e.target.value)
-    }
+const Register = observer(() => {
+    const {AuthStore} = useStores()
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
-        <div>
-            <h2>{AuthStore.values.username}</h2>
-            <input onChange={onChange}/>
-        </div>
+        <Wrapper>
+            <h1>注册</h1>
+            <Forms>
+                <Form
+                    name="basic"
+                    labelCol={{span: 8}}
+                    wrapperCol={{span: 8}}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="用户名"
+                        name="username"
+                        rules={[{required: true, message: '请输入用户名'}]}
+                    >
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="密码"
+                        name="passwordFirst"
+                        rules={[{required: true, message: '请输入密码'}]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="确认密码"
+                        name="passwordSecond"
+                        rules={[{required: true, message: '请再次输入密码'}]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{offset: 11, span: 8}}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Forms>
+        </Wrapper>
     )
 })
+const Wrapper=styled.div`
+  border:1px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Forms=styled.div`
+  border:1px solid black;
+  width:50%;
+`
 
 export default Register
